91b5b3dbedf76d81fd487f8879d17cdb4d09047c
sam2002jain <jainsam1975@gmail.com>
2024-12-23 19:35:19 +0530

    changes 1.0.0

--- index.js
+++ index.js
@@ -2,23 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
 // MongoDB Connection
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/alphaBackend`, {
-    });
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1);
-  }
-})();
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch((error) => {
+  console.error('MongoDB connection error:', error);
+  process.exit(1);
+});
+
 
 // User Schema
 const userSchema = new mongoose.Schema({
