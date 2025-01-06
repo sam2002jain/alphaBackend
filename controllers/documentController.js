8dac5ebe01f06d3eadb4aff1883e169be49e428d
sam2002jain <jainsam1975@gmail.com>
2025-01-07 00:04:21 +0530

    changed to post from get

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -1,23 +1,27 @@
-const Document = require('../models/documentModel')
+const Document = require('../models/documentModel');
 
-const document = async (req, res)=>{
-    try{
-    const{title} = req.body;
+const addDocument = async (req, res) => {
+  try {
+    const { title } = req.body;
 
-    const document = new Document({title});
-    await document.save();
-
-    res.status(201).json({
-        message:'Document added!',
-        document:{
-            id:document._id,
-            title:document.title,
-        },
-    });
-    }catch(error){
-        console.log('document failed to upload', error);
-        res.status(500).json({error:'server error'});
+    if (!title) {
+      return res.status(400).json({ error: 'Title is required' });
     }
 
+    const newDocument = new Document({ title });
+    await newDocument.save();
+
+    res.status(201).json({
+      message: 'Document added!',
+      document: {
+        id: newDocument._id,
+        title: newDocument.title,
+      },
+    });
+  } catch (error) {
+    console.error('Document failed to upload:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
 };
-module.exports = { document };
+
+module.exports = { addDocument };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const { signUp, signIn } = require('../controllers/authController');
-const { document } = require('../controllers/documentController');
-
+const { addDocument } = require('../controllers/documentController');
 
 const router = express.Router();
 
 // Routes
 router.post('/signup', signUp);
 router.post('/signin', signIn);
-router.post('/docdata',document);
+router.post('/docdata', addDocument);
 
 module.exports = router;
